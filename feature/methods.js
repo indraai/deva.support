b7a29438edf1878d2dfbb896dec1c08fb0230f8d
Quinn <quinn@indra.ai>
2023-06-03 14:00:17 -0700

    Update deva.world core to latest version

--- feature/methods.js
+++ feature/methods.js
@@ -30,4 +30,61 @@ module.exports = {
       })
     });
   },
+  
+  /**************
+  method: status
+  params: packet
+  describe: Return the current status of the Log Buddy.
+  ***************/
+  status(packet) {
+    this.context('status');
+    return Promise.resolve(this.status());
+  },
+  /**************
+  method: info
+  params: packet
+  describe: Return the current info for the deva.
+  ***************/
+  info(packet) {
+    return Promise.resolve(this.info);
+  },
+  /**************
+  method: issue
+  params: packet
+  describe: create a new issue for the main deva.world through github agent.
+  ***************/
+  issue(packet) {
+    return new Promise((resolve, reject) => {
+      this.question(`#github create_issue:${packet.q.agent.key} ${packet.q.text}`).then(issue => {
+        return resolve({
+          text: issue.a.text,
+          html: issue.a.html,
+          data: issue.a.data,
+        })
+      }).catch(err => {
+        return this.error(err, packet, reject);
+      });
+    });
+  },
+  /**************
+  method: help
+  params: packet
+  describe: The Help method returns the information on how to use the Log Buddy.
+  ***************/
+  help(packet) {
+    this.context('help');
+    const {dir} = this.info();
+    console.log('INFO', this.info());
+    return new Promise((resolve, reject) => {
+      this.help(packet.q.text, dir).then(help => {
+        return this.question(`#feecting parse ${help}`);
+      }).then(parsed => {
+        return resolve({
+          text: parsed.a.text,
+          html: parsed.a.html,
+          data: parsed.a.data,
+        });
+      }).catch(reject);
+    });
+  }
 }
